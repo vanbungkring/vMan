aa77c3dc5d69c706c4b4492057668bfaacccacb4
Arie Prasetyo <vanbungkring@Aries-MacBook-Pro.local>
2015-03-31 01:16:23 +0700

    Initial Commit

---
+++ vMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vMan
+//
+//  Created by Arie Prasetyo on 3/31/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vMan
+//
+//  Created by Arie Prasetyo on 3/31/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
